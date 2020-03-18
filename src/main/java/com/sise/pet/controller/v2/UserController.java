bfc246eef9a680f2e4c2c2b2f325921357bb9e08
dzq <1341773535@qq.com>
2020-03-18 23:54:23 +0800

    增加用户相关接口

--- src/main/java/com/sise/pet/controller/v2/UserController.java
+++ src/main/java/com/sise/pet/controller/v2/UserController.java
@@ -1,15 +1,16 @@
 package com.sise.pet.controller.v2;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.sise.pet.core.Result;
 import com.sise.pet.core.ResultGenerator;
+import com.sise.pet.entity.User;
+import com.sise.pet.exception.RedisConnectException;
 import com.sise.pet.service.IUserService;
 import com.sise.pet.service.RedisService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -20,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2020-02-27
  */
 @RestController
-@RequestMapping("/api/v1/user")
+@RequestMapping(value = {"/api/v1/user","/api/v2/user"})
 public class UserController {
 
     @Autowired
@@ -30,19 +31,60 @@ public class UserController {
     private RedisService redisService;
 
     /**
-     * 判断旧密码是否正确
+     * 修改密码
      * @param account
-     * @param password
+     * @param newPassword
+     * @param captcha
      * @return
      */
-    @GetMapping("/password/check")
-    public Result checkOldPassword(String account, String password) {
-        Boolean code = userService.checkOldPassword(account, password);
-        return ResultGenerator.genSuccessResult(code);
+    @PutMapping("/updatePassword")
+    public Result updatePassword(String account,String newPassword,String captcha){
+        try {
+            String redisCaptcha = redisService.get("captcha:" + account);
+            if(StringUtils.isNotEmpty(redisCaptcha)){
+                //缓存中的验证码
+                if(StringUtils.equals(redisCaptcha,captcha)){
+                    //验证码正确
+                    User user = new User();
+                    user.setPassword(newPassword);
+                    QueryWrapper<User> wrapper = new QueryWrapper();
+                    wrapper.eq("account", account);
+                    userService.update(user, wrapper);
+                    return ResultGenerator.genSuccessResult();
+                }else{
+                    return ResultGenerator.genFailResult("验证码错误或已过期");
+                }
+            }else{
+                return ResultGenerator.genFailResult("验证码错误或已过期");
+            }
+        } catch (RedisConnectException e) {
+            e.printStackTrace();
+            return ResultGenerator.genFailResult("获取验证码失败");
+        }
     }
 
+    /**
+     * 获取某个用户的信息
+     * @param userId
+     * @return
+     */
+    @GetMapping("/{userId}")
+    public Result get(@PathVariable Integer userId){
+        User user = userService.getById(userId);
+        user.setPassword("it's a secret");
+        return ResultGenerator.genSuccessResult(user);
+    }
+
+    /**
+     * 更新用户信息
+     * @param user
+     * @return
+     */
     @PutMapping
-    public Result updatePassword(String id,String newPassword,String captcha){
+    public Result updateUser(User user){
+        userService.updateById(user);
         return ResultGenerator.genSuccessResult();
     }
+
+
 }
